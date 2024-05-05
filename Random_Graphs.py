a0ba959599390bcb10b38b32ae625b56e3c32402
Lukas Schaller <schaller.lu@gmx.de>
2024-05-05 14:18:19 +0200

    Improve graph display for sparse graphs

--- Random_Graphs.py
+++ Random_Graphs.py
@@ -5,11 +5,11 @@ from utils.p_functions import p_functions
 from utils.graph_generation import generate_expected_graph
 
 
-def display_graph(graph):
+def display_graph(graph, n):
     fig, ax = plt.subplots()
-    nx.draw(graph, with_labels=True, ax=ax)
+    pos = nx.spring_layout(graph, k=0.4)
+    nx.draw(graph, pos,  with_labels=False, node_size=int(4000 / n),  ax=ax, node_color='indigo', edge_color='black', alpha=0.8)
     st.pyplot(fig)
-
 
 
 def main():
@@ -23,24 +23,31 @@ def main():
     st.info("""
     This app generates [Erdős–Rényi random graphs](https://en.wikipedia.org/wiki/Erdős–Rényi_model) and calculates the
     expected graph based on the average degree sequence.
-    Use the sidebar to control the number of nodes and the probability function to control the look of the graph.
+    Use the sidebar to control the number of nodes and the probability function to control the look of the graph. \n
+    Use Property Analysis to analyze the expected properties of a graph.
     """)
+
+    st.sidebar.title("Configuration")
 
     # Sidebar controls
     n = st.sidebar.slider("Number of Nodes", 10, 100, 20)
 
     # Select p function
-    selected_function = st.sidebar.selectbox("Select Threshold Function", list(p_functions.keys()), index=3)
+    selected_function = st.sidebar.selectbox("Select Threshold Function", list(p_functions.keys()), index=5)
 
     # Calculate probability based on selected function and display
     p = p_functions[selected_function](n)
     st.sidebar.write(f"Probability (p) based on {selected_function}: {p:.4f}")
 
+    # Regenerate graph button
+    if st.sidebar.button("Regenerate Graph"):
+        st.rerun()
+
     # Generate a random graph
     graph = generate_expected_graph(n, p)
 
     # Display the graph
-    display_graph(graph)
+    display_graph(graph, n)
 
 
 if __name__ == "__main__":
